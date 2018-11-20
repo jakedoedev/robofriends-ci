8d35481694e2ee0e75608d09445073b7d0e88e07
Martin Louie Dela Serna <mldelaserna@Martins-MacBook-Pro.local>
2018-11-20 23:06:13 +0800

    Add counter2 component and prettier test

---
+++ src/components/CounterButton2.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class CounterButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.count !== nextState.count) {
+      return true;
+    }
+    return false;
+  }
+
+  updateCount = () => {
+    this.setState(state => {
+      return { count: state.count + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <button
+        id="counter"
+        className="f6 link dim ba bw2 ph3 pv2 mb2 dib purple"
+        color={this.props.color}
+        onClick={this.updateCount}
+      >
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+
+export default CounterButton;
